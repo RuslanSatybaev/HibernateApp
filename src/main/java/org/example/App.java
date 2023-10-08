d04e6b23464ad99f7887bc3c7abd7ba467172f5e
Ruslan Satybaev <medtehnik850@gmail.com>
2023-10-08 22:33:07 +0700

    Lesson3: Add items

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.models.Item;
 import org.example.models.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -10,11 +11,17 @@ import org.hibernate.cfg.Configuration;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
-            session.createQuery("DELETE Person WHERE age < 30").executeUpdate();
+
+            Person person = session.get(Person.class, 4);
+            Item item = session.get(Item.class, 1);
+            item.getOwner().getItems().remove(item);
+            item.setOwner(person);
+            person.getItems().add(item);
 
             session.getTransaction().commit();
         }
--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,6 +1,7 @@
 package org.example.models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "person")
@@ -16,6 +17,9 @@ public class Person {
 
     @Column(name = "age")
     private int age;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Item> items;
 
     public Person() {
     }
@@ -48,6 +52,14 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 
     @Override
