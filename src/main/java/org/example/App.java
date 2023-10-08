2774a997e3329eff8039fe54e07e49f36154b4bd
Ruslan Satybaev <medtehnik850@gmail.com>
2023-10-08 23:47:32 +0700

    Lesson4: Add cascade

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,9 +6,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
@@ -17,11 +14,11 @@ public class App {
              Session session = sessionFactory.getCurrentSession()) {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 4);
-            Item item = session.get(Item.class, 1);
-            item.getOwner().getItems().remove(item);
-            item.setOwner(person);
-            person.getItems().add(item);
+            Person person = new Person("Test cascading", 35);
+            person.addItem(new Item("Test cascading item"));
+            person.addItem(new Item("Test cascading item2"));
+            person.addItem(new Item("Test cascading item3"));
+            session.save(person);
 
             session.getTransaction().commit();
         }
